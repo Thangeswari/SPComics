135df57535f565fb1c0daedb3155b72c32372ffe
Thangeswari <spthangu2016@gmail.com>
2018-10-01 20:12:48 +0530

    JS Auto Code Generation
    View Site Properties

--- JSCodeFromPSSites.ps1
+++ JSCodeFromPSSites.ps1
@@ -1,4 +1,4 @@
-﻿function CreateJSScriptSiteDetails($SiteURL,$User,$loadObject,$SpProperty){
+﻿function CreateJSScriptSiteDetails($SiteURL,$User,$loadObject,$SpProperty,$propertyName,$MultiResult,$itemProperty){
 
 #Download Sharepoint online client components
 
@@ -20,20 +20,36 @@ $enc = [system.Text.Encoding]::UTF8
 
 $sb = [System.Text.StringBuilder]::new()
 [void]$sb.Append( '<script type="text/ecmascript" language="ecmascript">' )
-[void]$sb.AppendLine("var "+$loadObject+";");
 [void]$sb.AppendLine("var site;");
+[void]$sb.AppendLine("var items;var item;var info;");
 [void]$sb.AppendLine("var clientContext;");
 [void]$sb.AppendLine("function getSite"+$loadObject+"(){");
 [void]$sb.AppendLine("this.clientContext = new SP.ClientContext.get_current();");
 [void]$sb.AppendLine("if (this.clientContext != undefined && this.clientContext != null) {");
-[void]$sb.AppendLine(""+$loadObject+"= clientContext.get_site()."+$SpProperty+";");
-[void]$sb.AppendLine("this.clientContext.load("+$loadObject+");");
+[void]$sb.AppendLine("this.site= clientContext.get_site();");
+if($MultiResult -eq $true){
+[void]$sb.AppendLine("this.clientContext.load(this.site);");
+[void]$sb.AppendLine("items= site."+$SpProperty+";");
+[void]$sb.AppendLine("this.clientContext.load(items);");
+}else{
+[void]$sb.AppendLine("this.clientContext.load(this.site,'"+$loadObject+"');");
+}
 [void]$sb.AppendLine("this.clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceeded), Function.createDelegate(this, this.onQueryFailed));");
 [void]$sb.AppendLine("}");
 [void]$sb.AppendLine("}");
 [void]$sb.AppendLine("function onQuerySucceeded() {");
-[void]$sb.AppendLine("var info = '"+$propertyName+": ' + "+$loadObject+";");
+if($MultiResult -eq $true){
+[void]$sb.AppendLine('var listEnumerator = items.getEnumerator();');
+[void]$sb.AppendLine("info = '"+$propertyName+"::';");
+[void]$sb.AppendLine('while (listEnumerator.moveNext()) {');
+[void]$sb.AppendLine('item = listEnumerator.get_current();');
+[void]$sb.AppendLine("info += item."+$itemProperty+"+'</br>';");
+[void]$sb.AppendLine('}');
+}else{
+[void]$sb.AppendLine("info = '"+$propertyName+"::'+ this.site."+$SpProperty+";");
+}
 [void]$sb.AppendLine("alert(info);");
+[void]$sb.AppendLine('document.getElementById("getSiteData").innerHTML="<b>"+info+"</b>";');
 [void]$sb.AppendLine("console.log(info);");
 [void]$sb.AppendLine("}");
 [void]$sb.AppendLine("function onQueryFailed(sender, args) {");
@@ -41,6 +57,7 @@ $sb = [System.Text.StringBuilder]::new()
 [void]$sb.AppendLine("}");
 [void]$sb.AppendLine("</script>");
 [void]$sb.AppendLine("<input id='Button1' type='button' value='Get Site"+$loadObject+" Details' onclick='getSite"+$loadObject+"();' />");
+[void]$sb.AppendLine('<div id="getSiteData"></div>');
 $sb.ToString()
 
 $data1 = $enc.GetBytes($sb.ToString()) 
