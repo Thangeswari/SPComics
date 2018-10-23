d23f8bccd79ef9a88863d69d47cfc01b783456f6
Thangeswari <spthangu2016@gmail.com>
2018-10-23 22:25:06 +0530

    Added PnP and Rest files

---
+++ PnPGarden.ps1
@@ -0,0 +1,48 @@
+﻿#Ensure Latest SharePointPnPPowerShell* is installed
+Install-Module -Name "SharePointPnPPowerShellOnline"
+
+#Connect to your SharePoint Site.Replace with your url
+$siteurl = "https://xxx.sharepoint.com/sites/dev" 
+
+Connect-PnPOnline –Url $siteUrl –Credentials (Get-Credential)
+
+#Create a Garden subsite
+New-PnPWeb -Title "GardenPnP" -Url "GardenPnP" -Description "Garden Subsite PnP" -Template "STS#0"
+
+#Update the URL
+$gardenUrl=$siteUrl+"/GardenPnP"
+Connect-PnPOnline –Url $gardenUrl –Credentials (Get-Credential)
+(Get-PnPContext).Url
+
+#Create a List Named Fruits
+New-PnPList -Title "Fruits" -Template GenericList
+
+#Create a column named Location of Text or GeoLocation type
+Add-PnPField -List "Fruits" -DisplayName "Location" -InternalName "Location" -Type Text -AddToDefaultView
+
+#Add 5 items to Fruits List
+$fruits="Apple","Orange","Mango","Kiwi","Pomogranate"
+$location="Washington"
+foreach ($item in $fruits)
+{    
+    $newItem = Add-PnPListItem -List Fruits
+    Set-PnPListItem -List "Fruits"  -Identity $newItem -Values @{"Title"="$($item)";"Location"="$($location)";}  
+}
+
+#Read the Fruit Details.
+Get-PnPListItem -List "Fruits"
+
+#Find Apple
+$appleItem=Get-PnPListItem -List Fruits -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value 
+    Type='Text'>Apple</Value></Eq></Where></Query></View>"
+
+#Update the Place for Apple from Washington to Australia.
+Set-PnPListItem -List "Fruits" -Identity $appleItem -Values @{"Title"="Apple";"Location"="Australia"}
+
+#Find Mango
+$mangoItem=Get-PnPListItem -List Fruits -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value 
+    Type='Text'>Mango</Value></Eq></Where></Query></View>"
+
+#Delete Mango
+Remove-PnPListItem -List Fruits -Identity $mangoItem
+
---
+++ RESTFairyGarden.ps1
@@ -0,0 +1,73 @@
+﻿#Replace the URL with your URL for targetSite and your User for User
+#Create a Garden Subsite
+$data=@"
+{'parameters':{'__metadata': { 'type': 'SP.WebInfoCreationInformation' },'Url': 'Garden','Title': 'Garden', 'Description': 'Garden web','Language': 1033,'WebTemplate': 'sts','UseUniquePermissions': false}}
+"@
+$restUrl="/_api/web/webinfos/add"
+$targetSite="https://xxx.sharepoint.com/sites/dev"
+$User="admin@xxx.onmicrosoft.com"
+$siteNew=Create-SPObject -targetSite $targetSite -User $User -restUrl $restUrl -data $data
+
+#Create a list named fruits
+$data=@"
+{ '__metadata': { 'type': 'SP.List' }, 'AllowContentTypes': true, 'BaseTemplate': 100,
+    'ContentTypesEnabled': true, 'Description': 'My Fruits List', 'Title': 'Fruits' }
+"@
+$targetSite="https://xxx.sharepoint.com/sites/dev/Garden"
+$User="admin@xxx.onmicrosoft.com"
+$restUrl="/_api/web/lists"
+$listNew=Create-SPObject -targetSite $targetSite -User $User -restUrl $restUrl -data $data
+
+#Create a column Location
+$data=@"
+{ '__metadata': { 'type': 'SP.Field' }, 'Title': 'Location', 'FieldTypeKind': 2 }
+"@
+$listName="Fruits"
+$restUrl="/_api/web/lists/getbytitle('$($listName)')/fields"
+$listFieldNew=Create-SPObject -targetSite $targetSite -User $User -restUrl $restUrl -data $data
+
+#Add five items
+$items="Apple","Orange","Mango","Kiwi","Pomogranate"
+$location="Washington"
+$listName="Fruits"
+$restUrl="/_api/web/lists/GetByTitle('"+$listName+"')/items"
+foreach ($item in $items)
+{
+    $data=@"
+{ '__metadata': { 'type': 'SP.Data.$($listName)ListItem' }, 'Title': '$($item)','Location':'$($location)' }
+"@
+$listItemNew=Create-SPObject -targetSite $targetSite -User $User -restUrl $restUrl -data $data
+}
+
+
+#Read List Items
+$restUrl="/_api/web/lists/getbytitle('$($listName)')/items"
+$listsFruits=Read-SPObject -targetSite $targetSite -User $User -restUrl $restUrl
+$results = $listsFruits.ToString().Replace("ID", "_ID") | ConvertFrom-Json
+$results.d.results|select Title,Location,Id
+
+#Read the Apple item
+$restUrl="/_api/web/lists/getbytitle('$($listName)')/items?`$filter=Title eq 'Apple'"
+$listsFruits=Read-SPObject -targetSite $targetSite -User $User -restUrl $restUrl
+$results = $listsFruits.ToString().Replace("ID", "_ID") | ConvertFrom-Json
+$results.d.results|select Title,Location,Id
+$etag=$results.d.results[0].__metadata.etag
+$listItemID=$results.d.results[0].id
+
+#Update the item
+$UpdatedValue="Australia"
+$restUrl="/_api/web/lists/GetByTitle('"+$listName+"')/items("+$listItemID+")"
+$data="{'__metadata': { 'type': 'SP.Data.$($listName)ListItem' }, 'Location': '$($UpdatedValue)'}"
+$updatedItem=Update-SPObject -targetSite $targetSite -User $User -restUrl $restUrl -data $data -etag $etag 
+
+#Read the Mango item
+$restUrl="/_api/web/lists/getbytitle('$($listName)')/items?`$filter=Title eq 'Mango'"
+$listsFruits=Read-SPObject -targetSite $targetSite -User $User -restUrl $restUrl
+$results = $listsFruits.ToString().Replace("ID", "_ID") | ConvertFrom-Json
+$results.d.results|select Title,Location,Id
+$etag=$results.d.results[0].__metadata.etag
+$deletelistItemID=$results.d.results[0].id
+
+#Delete the item
+$restUrl="/_api/web/lists/GetByTitle('"+$listName+"')/items("+$deletelistItemID+")"
+$listItemdeleted=Delete-SPObject -targetSite $targetSite -User $User -restUrl $restUrl
